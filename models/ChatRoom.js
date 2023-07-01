1c6d241f7972ca4f11364d899ae7147352eb992e
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-07-01 19:53:29 +0300

    Update ChatRoom.js

--- models/ChatRoom.js
+++ models/ChatRoom.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 const chatroomSchema = new Schema({
     name: { type: String, required: 'name is required' },
     commetntsCount: { type: Number, default: 0 },
-    user: { type: Schema.Types.ObjectId, required: 'user required', ref: 'User' },
+    // user: { type: Schema.Types.ObjectId, required: 'user required', ref: 'User' },
 },
     { timestamps: true, versionKey: false });
 const Chatroom = model('Chatroom', chatroomSchema);
