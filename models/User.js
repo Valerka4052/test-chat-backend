9db4adb25c8effc565393f4a7933d4b633cd0418
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-07-06 13:23:28 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const userSchema = new Schema({
     name: { type: String, required: 'name is required' },
     email: { type: String, required: 'email is required' },
     password: { type: String, required: 'password is required' },
-    imageURL: { type: String },
+    imageURL: { type: String,default:'' },
 },
     { timestamps: true, versionKey: false });
 const User = model('User', userSchema);
